2480e7fae7f29910b20d727ab79c2837a161cf36
236sec <popole159za@gmail.com>
2023-05-19 17:49:30 +0700

    Set MoviePoster to fill the container

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -20,15 +20,15 @@ export default function MovieCard(props) {
   return (
     <div className={`mx-3`} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
       <div className={`transition-all duration-300 relative w-64 h-[24rem]`}>
-        <div className="w-full h-full">
+        
           <Image
             alt="Poster"
             src={movie.Poster !== "N/A" ?  movie.Poster : "https://via.placeholder.com/400"}
             width={2000}
             height={3000}
-            className={`transition-all duration-300 ${isHover ? "rounded-none" : "rounded-lg"}`}
+            className={`transition-all duration-300 object-fill w-64 h-[24rem] ${isHover ? "rounded-none" : "rounded-lg"}`}
           />
-        </div>
+        
         <div className={`my-0 w-full h-2/6 absolute bottom-0 bg-black bg-opacity-50 transition-all duration-${isHover ? "500" : "100"}  ${isHover ? "opacity-100" : "opacity-0"}`}>
           <h1 className="text-3xl font-bold" >{movie.Title}</h1>
           <p className="text-lg">Year:{movie.year}</p>
