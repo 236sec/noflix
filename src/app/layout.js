31cb7bdf177e700b421cbf755fdb652651595f2d
236sec <popole159za@gmail.com>
2023-05-21 13:31:10 +0700

    Fixed Background

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth'>
-      <body className={`${inter.className} bg-gradient-to-bl from-slate-700 to-slate-900`} >
+      <body className={`${inter.className} bg-gradient-to-bl from-slate-700 to-slate-900 min-w-[100vh] min-h-screen`} >
         <div className='text-white m-0 p-0'>
           {children}
         </div>
