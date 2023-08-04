1aa02a130dab00209881bfb6dafab38c8e9fd953
MyMac <popole159za@gmail.com>
2023-08-04 13:30:25 +0700

    Fix NextjsConfig 2

--- next.config.js
+++ next.config.js
@@ -1,39 +1,18 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
+/** @type {import('next').NextConfig} */
 
-// Check if .env file exists before attempting to read it
-if (fs.existsSync('.env')) {
-  const env = dotenv.parse(fs.readFileSync('.env'));
-  // Use the parsed environment variables in the config
-  const nextConfig = {
+const nextConfig = {
     reactStrictMode: true,
-    env: env,
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-          port: '',
-          pathname: '**',
-        }
-      ],
-    },
-  };
-  module.exports = nextConfig;
-} else {
-  // Fallback if .env file doesn't exist
-  module.exports = {
-    reactStrictMode: true,
-    env: {},
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-          port: '',
-          pathname: '**',
-        }
-      ],
-    },
-  };
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',
+            port: '',
+            pathname: '**',
+          }
+        ],
+      },
 }
+
+module.exports = nextConfig
+
